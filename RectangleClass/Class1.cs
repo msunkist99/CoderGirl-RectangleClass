2f62bdd1042a4b24de0989a596f9a24ae2f4e843
msunkist99 <msunkist99@gmail.com>
2019-02-24 12:58:29 -0600

    2019/2/24 Initial Code

--- RectangleClass/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RectangleClass
-{
-    public class Class1
-    {
-    }
-}
---
+++ RectangleClass/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RectangleClass
+{
+    public class Rectangle
+    {
+
+        public int length { get; set; }
+        public int width { get; set; }
+
+        public Rectangle()
+        {
+            length = 0;
+            width = 0;
+        }
+
+        public double GetRectangleArea()
+        {
+            return length * width;
+        }
+
+        public double GetRectanglePerimeter()
+        {
+            return (length * 2) + (width * 2);
+        }
+
+        public bool IsRectangleASquare()
+        {
+            /*
+            if (length == width)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            */
+
+            return length == width ? true : false;
+        }
+    }
+}
+
